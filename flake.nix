f87137dab2778f54b0bd9f903d3c4e0060fb553f
Peter Marreck <lumbergh@gmail.com>
2026-02-15 12:43:10 -0500

    Add flake.nix with GMP, LLVM 19, clang 19

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "llvm-pi: compute pi in hand-written LLVM IR";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "aarch64-darwin" "x86_64-darwin" "aarch64-linux" "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.gmp
+              pkgs.gmp.dev
+              pkgs.llvmPackages_19.llvm
+              pkgs.clang_19
+            ];
+            shellHook = ''
+              export GMP_INCLUDE="${pkgs.gmp.dev}/include"
+              export GMP_LIB="${pkgs.gmp}/lib"
+            '';
+          };
+        });
+    };
+}
